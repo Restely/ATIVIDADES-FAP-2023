c3dd5fd0a8b6e54650376c30f513d34537319ce7
Samara Restely <139304808+Restely@users.noreply.github.com>
2023-08-05 20:05:04 -0300

    Add files via upload

---
+++ Lógica de Programação /Desafios/listas.js
@@ -0,0 +1,14 @@
+let listaNomes = ['Flávia', 'Camila', 'Maria', 'João', 'José', 'Pedro', 'Zenaide', 'Solange', 'Wesley', 'Xande']
+let idades = [29, 23, 25, 24, 17, 16, 19, 26, 30, 45]
+let coresFavoritas = [ 'Preto', 'Cinza', 'Azul', 'Verde', 'Roxo', 'Vermelho','Rosa', 'Laranja', 'Amarelo', 'Branco']
+
+console.log (listaNomes)
+console.log (idades)
+console.log (coresFavoritas)
+
+idades [0] = 30
+coresFavoritas[9]= 'Bege'
+
+console.log (listaNomes)
+console.log (idades)
+console.log (coresFavoritas)
