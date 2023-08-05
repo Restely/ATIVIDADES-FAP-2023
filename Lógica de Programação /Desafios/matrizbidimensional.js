447848c3c1434c39260848e2e24524060a0e968f
Samara Restely <139304808+Restely@users.noreply.github.com>
2023-08-05 20:33:03 -0300

    Add files via upload

---
+++ Lógica de Programação /Desafios/matrizbidimensional.js
@@ -0,0 +1,12 @@
+/*Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional para armazenar informações sobre animais e as suas características.
+
+Cada linha deve representar um animal e cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.
+*/
+
+var listaAnimais = Array()
+
+ listaAnimais ['Cachorro'] = ['Caramelo', 'Vira-lata', '7']
+listaAnimais ['Cavalo'] = ['Preto', 'AndaLuz', ' 10']
+listaAnimais['Gato'] = ['Amarelo', 'Persa', '3']
+
+console.log (listaAnimais)
