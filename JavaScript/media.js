67b686136b9bbca88acf181dba78f6314c68dabd
Samara Restely <139304808+Restely@users.noreply.github.com>
2023-10-09 21:57:07 -0300

    media.js

---
+++ JavaScript/media.js
@@ -0,0 +1,6 @@
+nota1 = parsefloat(prompt("Insira a primeira nota: ")
+nota2 = parsefloat(prompt("Insira a segunda nota: ")
+nota3 = parsefloat(prompt("Insira a terceira nota: ")
+media = (nota1 + nota2 + nota3)/3
+
+console.log("Sua média é: " + media)
